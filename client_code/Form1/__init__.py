b27c64ee40e133889d2fce1133a2d642bb05a1b8
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2023-11-02 15:12:04 +0000

    Created new_file.html, ServerModule1, Form1.ItemTemplate1 and new_file.css; Edited settings and Form1; Deleted theme.css and standard-page.html

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,13 +1,21 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+  
+    
+    
 
     # Any code you write here will run before the form opens.
 
-  def text_box_1_change(self, **event_args):
-    """This method is called when the text in this text box is edited"""
+  def image_1_mouse_enter(self, x, y, **event_args):
+    """This method is called when the mouse cursor enters this component"""
+    pass
+
+  def image_1_show(self, **event_args):
+    """This method is called when the Image is shown on the screen"""
     pass
