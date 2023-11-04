d714dfd7696937b3e9a67c96bec53dea081b66c0
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2023-11-04 07:16:04 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -22,4 +22,41 @@ class Form1(Form1Template):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    pregnancies = self.txtPregnancies.text
+    glucose = self.txtGlucose.text
+    blood_pressure = self.txtBloodPressure.text
+    skin_thickness = self.txtSkinThickness.text
+    insulin = self.txtInsulin.text
+    bmi = self.txtBMI.text
+    diabetes_pedigree_function = self.txtDiabetesPedigreeFunction.text
+    age = self.txtAge.text
+
+    # Create a dictionary with user input data
+    user_data = {
+        "Pregnancies": pregnancies,
+        "Glucose": glucose,
+        "BloodPressure": blood_pressure,
+        "SkinThickness": skin_thickness,
+        "Insulin": insulin,
+        "BMI": bmi,
+        "DiabetesPedigreeFunction": diabetes_pedigree_function,
+        "Age": age
+        # Add other input fields here
+    }
+
+    # Define the URL of your FastAPI endpoint
+    url = "http://your-fastapi-url/predict/"
+
+    # Make an HTTP POST request to your FastAPI endpoint
+    response = anvil.server.http.request(url, json=user_data)
+
+    # Handle the response from your FastAPI backend
+    if response.status_code == 200:
+        # If the response status code is 200 (OK), you can access the result
+        result = response.json()
+        # Do something with the result (e.g., display it to the user)
+    else:
+        # Handle the case where the request was not successful
+        # (you can display an error message to the user)
+   
+
