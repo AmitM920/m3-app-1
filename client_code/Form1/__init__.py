120327de197ab2f0b1016461e337e6315958ec0d
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2023-11-11 10:55:03 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -20,7 +20,7 @@ class Form1(Form1Template):
     """This method is called when the Image is shown on the screen"""
     pass
 
-  def button_1_click(self, **event_args):
+def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     pregnancies = self.txtPregnancies.text
     glucose = self.txtGlucose.text
@@ -45,18 +45,35 @@ class Form1(Form1Template):
     }
 
     # Define the URL of your FastAPI endpoint
-    url = "http://your-fastapi-url/predict/"
+    url = "http://localhost:8000/predict/"
 
-    # Make an HTTP POST request to your FastAPI endpoint
-    response = anvil.server.http.request(url, json=user_data)
+    try:
+        # Make an HTTP POST request to your FastAPI endpoint
+        response = requests.post(url, json=user_data)
 
-    # Handle the response from your FastAPI backend
-    if response.status_code == 200:
-        # If the response status code is 200 (OK), you can access the result
-        result = response.json()
-        # Do something with the result (e.g., display it to the user)
-    else:
-        # Handle the case where the request was not successful
-        # (you can display an error message to the user)
-   
+        # Handle the response from your FastAPI backend
+        if response.status_code == 200:
+            # If the response status code is 200 (OK), you can access the result
+            result = response.json()
+            # Display result to the user
+            self.label_result.text = f"Prediction: {result['prediction']}"
+            # Clear input fields
+            self.clear_input_fields()
+        else:
+            # Handle the case where the request was not successful
+            # Display an error message to the user
+            alert(f"Error: {response.status_code} - {response.text}")
+    except Exception as e:
+        alert(f"An error occurred: {str(e)}")
 
+def clear_input_fields(self):
+    """Clear input fields after submitting the form"""
+    self.txtPregnancies.text = ""
+    self.txtGlucose.text = ""
+    self.txtBloodPressure.text = ""
+    self.txtSkinThickness.text = ""
+    self.txtInsulin.text = ""
+    self.txtBMI.text = ""
+    self.txtDiabetesPedigreeFunction.text = ""
+    self.txtAge.text = ""
+    # Clear other input fields if needed
