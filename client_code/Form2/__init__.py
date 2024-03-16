c7552c032564a70df772e37bb53b4da5cc7d80cb
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2024-03-16 11:26:43 +0000

    Created Form2; Edited settings, ServerModule1, Form1.ItemTemplate1 and Form1

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,13 +1,13 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
-class ItemTemplate1(ItemTemplate1Template):
+class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
