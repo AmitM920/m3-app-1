de2ce27d82165d3b298c8480dc1fc4611e402f2a
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2024-03-17 16:52:16 +0000

    Created login; Edited settings, ServerModule1, content_page and content_page.ItemTemplate1; Deleted registration_page

--- client_code/login/__init__.py
+++ client_code/login/__init__.py
@@ -1,16 +1,18 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import loginTemplate
 from anvil import *
-import anvil.users
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
+import anvil.users
 
-class ItemTemplate1(ItemTemplate1Template):
+
+class login(loginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,9 +1,11 @@
+import anvil.users
 import anvil.google.auth, anvil.google.drive, anvil.google.mail
 from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -12,12 +14,13 @@ import anvil.server
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
 #
-@anvil.server.callable
+
 # def say_hello(name):
 #   print("Hello, " + name + "!")
 #   return 42
+
+@anvil.server.callable
 def register_user_with_google(email, display_name):
     # Implement logic to register the user using the provided email and display name
     # You can store the user information in your database or perform any additional tasks required for registration
     app_tables.users.add_row(email=email, display_name=display_name)
-    pass
