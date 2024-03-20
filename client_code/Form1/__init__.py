d50e5eb46ff4f2d6ac7a20c4609746ae7601812a
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2024-03-20 07:35:39 +0000

    Created new_file.html and Form1; Edited content_page and login

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import loginTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -8,16 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class login(loginTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
-  def button_1_show(self, **event_args):
-   """This method is called when the Button is shown on the screen"""
-   open_form('content_page')
-  pass
+
     # Any code you write here will run before the form opens.
-
-
-       
