f3950203b5023455825e9adc1945e78cd50a3d5b
amitmehta9202000@gmail.com <amitmehta9202000@gmail.com>
2024-05-21 06:33:31 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -7,18 +7,6 @@ from anvil.tables import app_tables
 import anvil.server
 
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-
 @anvil.server.callable
 def register_user_with_google(email, display_name):
     # Implement logic to register the user using the provided email and display name
